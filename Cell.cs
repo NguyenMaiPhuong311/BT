1d13b02db0047fe5a85ad82878d7264ccdafea02
NguyenMaiPhuong311 <146962157+NguyenMaiPhuong311@users.noreply.github.com>
2023-11-20 20:03:41 +0700

    n

---
+++ Cell.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Duan2
+{
+    public enum FIELD { FLD_EMPTY=' ',FLD_X='X', FLD_O='O'}
+    internal class Cell
+    {
+        FIELD fieldState = FIELD.FLD_EMPTY;
+        public Cell(FIELD fieldState = FIELD.FLD_EMPTY)
+        {
+            this.fieldState = fieldState;
+        }
+
+        public FIELD FieldState { get => fieldState; set => fieldState = value; }
+        //kiểm tra trống không
+        public bool isEmpty()
+        {
+            return (fieldState == FIELD.FLD_EMPTY);
+        }
+        public void markField(char c)
+        {
+            if (c == 'X')
+                fieldState = FIELD.FLD_X;
+            else if  (c == 'O')
+                fieldState = FIELD.FLD_O;
+            else
+                fieldState = FIELD.FLD_EMPTY;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿namespace Duan2
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            TicTacToe game = new TicTacToe();
+            game.play();
+
+            Console.ReadKey();
+        }
+    }
+}
