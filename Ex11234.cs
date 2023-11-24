66e44984ff3327a14a90434c1d630ed0821d001d
NguyenMaiPhuong311 <146962157+NguyenMaiPhuong311@users.noreply.github.com>
2023-11-24 08:33:30 +0700

    final

---
+++ Ex11234.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Ex11234
+    {
+        static void Main(string[] args)
+        {
+            using (var reader = new StreamReader(@"C:\test.csv"))
+            {
+                List<string> listA = new List<string>();
+                List<string> listB = new List<string>();
+                while (!reader.EndOfStream)
+                {
+                    var line = reader.ReadLine();
+                    var values = line.Split(';');
+
+                    listA.Add(values[0]);
+                    listB.Add(values[1]);
+                }
+            }
+        }
+    }
+}
